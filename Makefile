68e44aa08fd33f8eb2891316a12730f5fdb4599e
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2025-12-22 10:34:02 +0300

    update version

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
-ROCM_GPU ?= $(strip $(shell rocminfo | grep -m 1 -E gfx[^0]{1} | sed -e 's/ *Name: *//'))
-ifeq ($(ROCM_GPU), gfx1030)
-  HSA_OVERRIDE_GFX_VERSION = 10.3.0
-else ifeq ($(ROCM_GPU), gfx1100)
-  HSA_OVERRIDE_GFX_VERSION = 11.0.0
-else
-  HSA_OVERRIDE_GFX_VERSION = 9.0.0
-endif
-CUR_VERSION = 2.9.1
+include hsa_override.mk
+
+CUR_VERSION = 2.10.0
 CONDA_DIR = $(PWD)/data/miniconda_v$(CUR_VERSION)
 
 build:
---
+++ hsa_override.mk
@@ -0,0 +1,47 @@
+GPU_INFO := $(strip $(shell lspci -nnk | grep -i -E "VGA|3D|Display"))
+
+ROCM_GPU := gfx900
+HSA_OVERRIDE_GFX_VERSION := 9.0.0
+
+ifneq (,$(or $(findstring RX 9070,$(GPU_INFO)),$(findstring R9700,$(GPU_INFO))))
+  ROCM_GPU := gfx1201
+  HSA_OVERRIDE_GFX_VERSION := 12.0.1
+endif
+
+ifneq (,$(findstring RX 9060,$(GPU_INFO)))
+  ROCM_GPU := gfx1200
+  HSA_OVERRIDE_GFX_VERSION := 12.0.0
+endif
+
+ifneq (,$(or $(findstring RX 7900,$(GPU_INFO)),$(findstring W7900,$(GPU_INFO)),$(findstring W7800,$(GPU_INFO))))
+  ROCM_GPU := gfx1100
+  HSA_OVERRIDE_GFX_VERSION := 11.0.0
+endif
+
+ifneq (,$(or $(findstring RX 7800,$(GPU_INFO)),$(findstring RX 7700,$(GPU_INFO)),$(findstring V710,$(GPU_INFO)),$(findstring W7700,$(GPU_INFO))))
+  ROCM_GPU := gfx1101
+  HSA_OVERRIDE_GFX_VERSION := 11.0.1
+endif
+
+ifneq (,$(or $(findstring W6800,$(GPU_INFO)),$(findstring V620,$(GPU_INFO)),$(findstring RX 6800,$(GPU_INFO)),$(findstring RX 6900,$(GPU_INFO))))
+  ROCM_GPU := gfx1030
+  HSA_OVERRIDE_GFX_VERSION := 10.3.0
+endif
+
+ifneq (,$(findstring MI3,$(GPU_INFO)))
+  ROCM_GPU := gfx942
+  HSA_OVERRIDE_GFX_VERSION := 9.4.2
+endif
+
+ifneq (,$(findstring MI2,$(GPU_INFO)))
+  ROCM_GPU := gfx90a
+  HSA_OVERRIDE_GFX_VERSION := 9.0.10
+endif
+
+ifneq (,$(or $(findstring Radeon VII,$(GPU_INFO)),$(findstring MI50,$(GPU_INFO))))
+  ROCM_GPU := gfx906
+  HSA_OVERRIDE_GFX_VERSION := 9.0.6
+endif
+
+$(info Detected: [$(GPU_INFO)])
+$(info Result: ROCM_GPU=$(ROCM_GPU), HSA_OVERRIDE_GFX_VERSION=$(HSA_OVERRIDE_GFX_VERSION))
